842f480e6fe413aa2e8a9aa44acd02333632ca46
Winners20 <elijahisrael84@gmail.com>
2023-08-16 09:09:07 -0700

    my commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,19 +13,4 @@ int _islower(int c)
 	else
 		return (0);
 }
-	
-
-
-4-isalpha.c
-
-#include "main.h"
-
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
